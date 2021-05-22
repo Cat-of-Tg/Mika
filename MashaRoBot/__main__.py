b7276ec693c474a508f15ac40183ca7cfdaad631
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-05-22 13:31:56 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -114,7 +114,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("MashaRoBot.modules." + module_name)
+    imported_module = importlib.import_module("Aleena_Gbot.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 

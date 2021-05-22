3d94729605f08df46d7d159de97866f0c401b39e
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-05-22 18:45:28 +0530

    Update global_bans_sql.py

--- MashaRoBot/modules/sql/global_bans_sql.py
+++ MashaRoBot/modules/sql/global_bans_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from AsunaRobot.modules.sql import BASE, SESSION
+from MashaRoBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
 

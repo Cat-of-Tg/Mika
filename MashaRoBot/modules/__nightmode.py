94aaf89203427ca67cd5e95b231974bfebf1e9c9
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2021-05-30 11:13:31 +0530

    Update __nightmode.py

--- MashaRoBot/modules/__nightmode.py
+++ MashaRoBot/modules/__nightmode.py
@@ -129,7 +129,7 @@ async def job_close():
     for pro in chats:
         try:
             await tbot.send_message(
-              int(pro.chat_id), "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By Evlie**"
+              int(pro.chat_id), "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By @kid_of_telegram**"
             )
             await tbot(
             functions.messages.EditChatDefaultBannedRightsRequest(
@@ -151,7 +151,7 @@ async def job_open():
     for pro in chats:
         try:
             await tbot.send_message(
-              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered By Masha**"
+              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered By Zaira**"
             )
             await tbot(
             functions.messages.EditChatDefaultBannedRightsRequest(

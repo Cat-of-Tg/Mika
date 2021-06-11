2c362fb43cba3497d3a67104e35c10234dc6db0c
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-06-11 15:09:20 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -100,7 +100,7 @@ buttons = [
 
 
 HELP_STRINGS = """
-Hello am [ZAIRA](https://telegra.ph/file/98c2580708ce5e0b067f9.jpg) and here are my helpful commands let's go through this and if you have any doubts ask at my support group."""
+Hello am [ZAIRA](https://telegra.ph/file/e83f3cd84f703864544f7.jpg) and here are my helpful commands let's go through this and if you have any doubts ask at my support group."""
 
 
 DONATE_STRING = """No need.. I'm rich"""

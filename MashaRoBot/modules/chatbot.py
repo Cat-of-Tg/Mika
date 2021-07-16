dc313874f048ae2c3b6fe8f0ebe7f83740176875
Im-zeus <87465532+Im-zeus@users.noreply.github.com>
2021-07-16 18:56:24 +0530

    Create chatbot.py

--- MashaRoBot/modules/chatbot.py
+++ MashaRoBot/modules/chatbot.py
@@ -142,7 +142,7 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows ZAIRA to talk and provides a more interactive group chat experience.
+Chatbot utilizes the CoffeeHouse API and allows SYLVIE to talk and provides a more interactive group chat experience.
 *Commands:* 
 *Admins only:*
  ❍ /addchat*:* Enables Chatbot mode in the chat.

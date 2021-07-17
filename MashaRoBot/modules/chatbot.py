4180017d8d786d8a262200e26d6f10ca820c33d0
Im-zeus <87465532+Im-zeus@users.noreply.github.com>
2021-07-17 18:50:14 +0530

    Create chatbot.py

--- MashaRoBot/modules/chatbot.py
+++ MashaRoBot/modules/chatbot.py
@@ -176,7 +176,7 @@ dispatcher.add_handler(REMOVE_CHAT_HANDLER)
 dispatcher.add_handler(CHATBOT_HANDLER)
 dispatcher.add_handler(LIST_CB_CHATS_HANDLER)
 
-__mod_name__ = "CHATBOT"
+__mod_name__ = "V-C"
 __command_list__ = ["addchat", "rmchat", "listaichats"]
 __handlers__ = [
     ADD_CHAT_HANDLER,

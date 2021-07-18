1c1008d44ecf4483aab536165dbd5e021075a153
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-07-18 21:34:53 +0530

    Create musicplayer.py

---
+++ MashaRoBot/modules/musicplayer.py
@@ -0,0 +1,26 @@
+__mod_name__ = "ᴍᴜꜱɪᴄ ᴘʟᴀʏᴇʀ"
+
+__help__ = """
+=>> *Song Playing* 🎧 
+❍ /play <song name> - play song you requested
+❍ /dplay <song name> - play song you requested via deezer
+❍ /splay <song name> - play song you requested via jio saavn
+❍ /playlist - Show now playing list
+❍ /current - Show now playing
+❍ /song <song name> - download songs you want quickly
+❍ /search <query> - search videos on youtube with details
+❍ /deezer <song name> - download songs you want quickly via deezer
+❍ /saavn <song name> - download songs you want quickly via saavn
+❍ /video <song name> - download videos you want quickly
+=>> *Admins only*
+❍ /player - open music player settings panel
+❍ /pause - pause song play
+❍ /resume - resume song play
+❍ /skip - play next song
+❍ /end - stop music play
+❍ /userbotjoin - invite assistant to your chat
+❍ /admincache - Refresh admin list
+=>>   *Use*
+1) Add @MissSylvie_bot in your group to admin with all permission
+2) Add Assistant I'd to your group @Sylvieassistant  😝
+"""

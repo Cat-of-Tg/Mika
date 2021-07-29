ad33f6685136a01345f2e9e3ad175c690ddf2705
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-07-30 05:21:45 +0530

    Update cleaner.py

--- MashaRoBot/modules/cleaner.py
+++ MashaRoBot/modules/cleaner.py
@@ -220,13 +220,6 @@ def bluetext_ignore_list(update: Update, context: CallbackContext):
     return
 
 
-__help__ = """
-*Blue text cleaner* removed any made up commands that people send in your chat.
- ❍ /cleanblue <on/off/yes/no>*:* clean commands after sending
- ❍ /ignoreblue <word>*:* prevent auto cleaning of the command
- ❍ /unignoreblue <word>*:* remove prevent auto cleaning of the command
- ❍ /listblue*:* list currently whitelisted commands
-"""
 
 SET_CLEAN_BLUE_TEXT_HANDLER = CommandHandler("cleanblue", set_blue_text_must_click)
 ADD_CLEAN_BLUE_TEXT_HANDLER = CommandHandler("ignoreblue", add_bluetext_ignore)
